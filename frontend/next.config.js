d8d459c9628dd677f90533ab667b44acc9cce0fc
martinacho22 <martin@randomcurve.com>
2026-06-24 07:34:40 -0700

    Add Next.js config

---
+++ frontend/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.r2.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "**.s3.amazonaws.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
